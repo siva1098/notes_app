0dca1b2cde4fb4d175a52df394f0251012c88f9c
Sivanesan <sivadinesh1998@gmail.com>
2022-02-15 18:53:40 +0530

    Uodated App name and App Icon

--- lib/main.dart
+++ lib/main.dart
@@ -37,28 +37,3 @@ class _MyAppState extends State<MyApp> {
     );
   }
 }
-
-// class MyHomePage extends StatefulWidget {
-//   const MyHomePage({Key? key}) : super(key: key);
-//   @override
-//   State<MyHomePage> createState() => _MyHomePageState();
-// }
-
-// class _MyHomePageState extends State<MyHomePage> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: const Text('Notes'),
-//       ),
-//       body: const Center(
-//         child: Text('Hello'),
-//       ),
-//       floatingActionButton: const FloatingActionButton(
-//         onPressed: null,
-//         tooltip: 'Increment',
-//         child: Icon(Icons.add),
-//       ), // This trailing comma makes auto-formatting nicer for build methods.
-//     );
-//   }
-// }
